13772ddfc0951b137000c520b636bd3daa4b61d6
zihanx3 <zihanx3@illinois.edu>
2018-12-10 23:49:18 -0600

    autofillHints APICall Gson Show one concrete example and being sucessful!!! Exception dealed

--- app/src/main/java/com/example/jessieyang/chefschoice/MainActivity.java
+++ app/src/main/java/com/example/jessieyang/chefschoice/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.jessieyang.chefschoice;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -8,21 +7,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
 
